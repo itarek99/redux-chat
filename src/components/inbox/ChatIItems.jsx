6012aa782916bf9a0040569fadfff4dc6230d569
itarek99 <tarekulislam7060@gmail.com>
2023-08-17 12:46:41 +0600

    conversations sidebar completed

--- src/components/inbox/ChatIItems.jsx
+++ src/components/inbox/ChatIItems.jsx
@@ -1,28 +1,58 @@
+import gravatarUrl from "gravatar-url";
+import moment from "moment/moment";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { useGetConversationsQuery } from "../../features/conversations/conversationsApi";
+import Error from "./../ui/Error";
 import ChatItem from "./ChatItem";
 
 export default function ChatItems() {
+  const { user } = useSelector((state) => state.auth);
+  const { data: conversations, isLoading, isError } = useGetConversationsQuery(user?.email);
+
+  if (isLoading) {
     return (
-        <ul>
-            <li>
-                <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Saad Hasan"
-                    lastMessage="bye"
-                    lastTime="25 minutes"
-                />
-                <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Sumit Saha"
-                    lastMessage="will talk to you later"
-                    lastTime="10 minutes"
-                />
-                <ChatItem
-                    avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-                    name="Mehedi Hasan"
-                    lastMessage="thanks for your support"
-                    lastTime="15 minutes"
-                />
-            </li>
-        </ul>
+      <ul>
+        <li className="m-2 text-center">Conversations Loading...</li>
+      </ul>
     );
+  }
+  if (!isLoading && isError) {
+    return (
+      <ul>
+        <li className="m-2 text-center">
+          <Error message={"Something Went Wrong :("} />
+        </li>
+      </ul>
+    );
+  }
+  if (!isLoading && !isError && conversations?.length === 0) {
+    return (
+      <ul>
+        <li className="m-2 text-center">No Conversations Found</li>;
+      </ul>
+    );
+  }
+
+  return (
+    <ul>
+      {conversations?.length > 0 &&
+        conversations.map((conversation) => {
+          const participant = conversation.users.find((participantUser) => participantUser.email !== user.email);
+
+          return (
+            <li key={conversation.id}>
+              <Link to={`/inbox/${conversation.id}`}>
+                <ChatItem
+                  avatar={gravatarUrl(participant.email, { size: 100 })}
+                  name={participant.name}
+                  lastMessage={conversation.message}
+                  lastTime={moment(conversation.timestamp).fromNow()}
+                />
+              </Link>
+            </li>
+          );
+        })}
+    </ul>
+  );
 }
